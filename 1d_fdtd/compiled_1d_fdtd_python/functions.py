160c2dd0d10233cad8c320963ee564507eeb2eef
marvinmoyco <marvin.moyco@gmail.com>
2021-04-25 02:16:49 +0800

    Added compiled .py file for a more organized way of differentiating different 1D FDTD modes

---
+++ 1d_fdtd/compiled_1d_fdtd_python/functions.py
@@ -0,0 +1,123 @@
+from scipy import constants
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+#Defining the constants
+c_0 = constants.speed_of_light
+mu_0 = constants.mu_0
+epsilon_0 = constants.epsilon_0
+
+def gaussian_source(f_max,t_prop,delta_t,delta_z,c_0):
+    """
+    Gaussian Pulse Source that is used for injecting source in 1D FDTD.
+    """
+    #Set source permittivity and permeability
+    mu_src =1 #these parameters should be the material permittivity/permeability at the grid position of the source injection
+    epsilon_src = 1
+
+    #Computing source parameters
+    tau = 0.5/f_max
+    t_0 = 6*tau
+    
+    T = 12*tau + 5*t_prop #Total time of simulation
+    N_t = math.ceil(T/delta_t) #Number of time steps
+    t = np.linspace(0,N_t*delta_t,N_t)
+
+
+    n_src = np.sqrt(epsilon_src*mu_src)
+    deltaT = (n_src*delta_z/2*c_0)+(delta_t/2)
+
+    A = -np.sqrt(epsilon_src/mu_src)
+    x_E = (t - t_0)/tau
+    x_H = (t-t_0 + deltaT)/tau
+
+    Esrc = np.exp(-np.power(x_E,2))
+    Hsrc = A*np.exp(-np.power(x_H,2))
+
+    return Esrc,Hsrc,t,N_t
+
+def plot_single(x=0,y1=0,y2=0,size=[20,13],labels=["X-Axis","Y-Axis","Title"]):
+    """
+    Plot a vector in a single time step (snapshot in time)
+    """
+    plt.figure(figsize = size)
+    plt.xlabel = (labels[0])
+    plt.ylabel = (labels[1])
+    plt.title = (labels[2])
+    plt.plot(x,y1)
+    plt.plot(x,y2)
+    plt.show()
+
+
+
+def plot_fields(E_plot,H_plot,N_t,injection_point,title="",save=False):
+    plt.ion()
+    fig = plt.figure(1,[10,6])
+    ax = fig.add_subplot(111)
+    plt.ylabel('Value')
+    plt.axvline(x=injection_point, color = "grey", linestyle="--")
+    plt.xlabel('z (Space)')
+
+    lineE, = ax.plot(E_plot[0,:])
+    lineH, = ax.plot(H_plot[0,:])
+    fig.show()
+    for i in range(1,N_t):
+        #plt.ylim(-5,5)
+        plt.legend(handles = [lineE,lineH],labels=["Electric Field","Magnetic Field"])
+        print(f"Currently plotting Iteration step: {i}/{N_t}")
+        plt.title(f'FDTD 1-D {title} | Iteration step: {i}/{N_t}')
+        if save == True:
+            plt.savefig(f"photos/1d-fdtd{i}.jpeg")
+        lineE.set_ydata(E_plot[i,:])
+        lineH.set_ydata(H_plot[i,:])
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+
+
+
+#The FDTD Algorithm used here are in the Ey/Hx Modes
+
+def algo_no_source(Ey,Hx,N_z,m_Ey,m_Hx):
+    """
+    This Basic FDTD Algorithm uses the Dirichlet Boundary Condition and has no source excitation.
+    """
+ 
+    #Update Hx from Ey (loop in space)
+    for k in range(N_z -1): #Leave out the last cell @ index=N_z-1 for the boundary condition
+        Hx[k] = Hx[k] + m_Hx*(Ey[k+1] - Ey[k])
+    #Dirichlet Boundary Condition for Hx at the end of the grid
+    Hx[N_z-1] = Hx[N_z-1] + m_Hx*(0 - Ey[N_z-1])
+
+    #Dirichlet Boundary Condition for Ey at the start of the grid
+    Ey[0] = Ey[0] + m_Ey*(Hx[0]-0)
+    #Update Ey from Hx (loop in space)
+    for k in range(1,N_z):
+        Ey[k] = Ey[k] + m_Ey*(Hx[k]-Hx[k-1])
+
+    return Ey,Hx
+
+def algo_soft_pabc(Ey,Hx,N_z,m_Ey,m_Hx,z_low,z_high,e2,h2):
+    """
+    This Basic FDTD Algorithm uses the Perfect Absorbing Boundary Condition and has Soft Source excitation.
+    """
+    #Record H at Boundary
+    h2 = z_low.pop(0)
+    z_low.append(Hx[1])
+
+    #Update Hx from Ey (loop in space)
+    for k in range(N_z -1): #Leave out the last cell @ index=N_z-1 for the boundary condition
+        Hx[k] = Hx[k] + m_Hx*(Ey[k+1] - Ey[k])
+    # Perfect Absorbing Boundary Condition for Hx at the end of the grid
+    Hx[N_z-1] = Hx[N_z-1] + m_Hx*(e2 - Ey[N_z-1])
+
+    #Record E at Boundary
+    e2 = z_high.pop(0)
+    z_high.append(Ey[N_z-1])
+
+    # Perfect Absorbing Boundary Condition for Ey at the start of the grid
+    Ey[0] = Ey[0] + m_Ey*(Hx[0]-h2)
+    #Update Ey from Hx (loop in space)
+    for k in range(1,N_z):
+        Ey[k] = Ey[k] + m_Ey*(Hx[k]-Hx[k-1])
+
+    return Ey,Hx,z_low,z_high
---
+++ 1d_fdtd/compiled_1d_fdtd_python/main.py
@@ -0,0 +1,145 @@
+
+#Importing of necessary libraries
+
+from functions import *
+
+
+
+
+#User Input
+f_max = float(input("Enter the max frequency of the source excitation (in Hz): "))
+mode = int(input("Enter the mode of FDTD Algorithm: \n(1) Basic Algorithm, No source excitation\n(2) Algorithm with Hard Source\n(3) Algorithm with Soft Source\n(4) Algorithm with Soft Source (with PABC)\n(5) Algorithm with TF/SF (with PABC):"))
+#NOTE: The assumption in this code is that the computational domain is composed of free-space
+#There is no other material present in the domain so the material properties are that of the free space
+
+#Computing Grid Resolution
+n_max = 1 #Due to free space
+lambda_min = c_0/(f_max*n_max)
+N_lambda = 10
+delta_lambda = lambda_min/N_lambda
+d_min = 0.01
+delta_d = d_min/4 #The d_min is assumed to be 0.01 since there is no device in the domain
+delta_init = min(delta_lambda,delta_d)
+#Since there is currently no critical dimension in the domain, assume it is 0.7
+spacers = 15
+d_critical = 0.7
+N_z = math.ceil(d_critical/delta_init) + spacers
+delta_z = d_critical/N_z
+print("=====================================================================")
+print(f"Number of Yee cells: {N_z} cells\nLength of each cell (Delta_z): {delta_z} m")
+
+#Computing material properties
+mu_r = 1    #Due to free space
+epsilon_r = 1
+n_r = np.sqrt(mu_r*epsilon_r)
+
+#Computing Time step and source excitation
+n_bc = 1 #Refractive index at the boundaries (assume free space)
+delta_t = (n_bc*delta_z)/(2*c_0)
+t_prop = (n_r*N_z*delta_z)/c_0 #time it takes to propagate in the domain
+Esrc,Hsrc,t,N_t = gaussian_source(f_max,t_prop,delta_t,delta_z,c_0)
+injection_point = 100 #Set this before the device/model in the domain
+print("=====================================================================")
+print(f"Time step: {delta_t} seconds")
+print(f"Number of iterations: {N_t} steps")
+print(f"Time vector: {t.shape} [Shape]")
+print(f"E-Field (Source):{Esrc.shape}, H-Field (Source): {Hsrc.shape}")
+
+# Computing the update coefficients
+m_E = c_0*delta_t/(epsilon_0*delta_z) #This is assuming that every cell is in free space
+m_H = c_0*delta_t/(mu_0*delta_z)
+
+#Field initialization
+E = np.zeros((N_z))
+H = np.zeros((N_z))
+print("=====================================================================")
+print(f"Update coefficients: m_E = {m_E}, m_H = {m_H}")
+print(f"Field vectors: Ey: {E.shape}, Hx: {H.shape}")
+
+#Initialize Boundary Terms (For Perfect Absorbing Boundary Conditions)
+z_low = [0,0]
+z_high = [0,0]
+e2 = 0
+h2 = 0
+
+#Initialize save matrix
+E_plot = np.zeros((N_t,N_z))
+H_plot = np.zeros((N_t,N_z))
+
+
+#Check here what mode to use...
+
+if mode == 1: #Basic Algorithm, No source excitation
+    #Loop in time for Algorithm
+    for i in range(N_t):
+        E,H = algo_no_source(E,H,N_z,m_E,m_H)
+
+        #Save into matrix
+        E_plot[i,:] = E
+        H_plot[i,:] = H
+        print("=====================================================================")
+        print(f"Successfully computed field values! iteration: {i}/{N_t}")
+
+elif mode == 2: #Algorithm with Hard Source
+    #Loop in time for Algorithm
+    for i in range(N_t):
+        E,H = algo_no_source(E,H,N_z,m_E,m_H)
+        #Insert Source Excitation (Hard Source)
+        E[injection_point] = Esrc[i]
+        #Save into matrix
+        E_plot[i,:] = E
+        H_plot[i,:] = H
+        print("=====================================================================")
+        print(f"Successfully computed field values! iteration: {i}/{N_t}")
+
+elif mode == 3: #Algorithm with Soft Source
+    #Loop in time for Algorithm
+    for i in range(N_t):
+        E,H = algo_no_source(E,H,N_z,m_E,m_H)
+
+        #Inserting source excitation (Soft Source)
+        E[injection_point] = E[injection_point] + Esrc[i]
+
+        #Save into matrix
+        E_plot[i,:] = E
+        H_plot[i,:] = H
+        print("=====================================================================")
+        print(f"Successfully computed field values! iteration: {i}/{N_t}")
+
+elif mode == 4: #Algorithm with Soft Source (with PABC)
+    #Loop in time for Algorithm
+    for i in range(N_t):
+        E,H,z_low,z_high = algo_soft_pabc(E,H,N_z,m_E,m_H,z_low,z_high,e2,h2)
+
+        #Inserting source excitation (Soft Source)
+        E[injection_point] = E[injection_point] + Esrc[i]
+
+        #Save into matrix
+        E_plot[i,:] = E
+        H_plot[i,:] = H
+        print("=====================================================================")
+        print(f"Successfully computed field values! iteration: {i}/{N_t}")
+
+
+elif mode == 5: #Algorithm with TF/SF (with PABC)
+    #Loop in time for Algorithm
+    for i in range(N_t):
+        E,H = algo_no_source(E,H,N_z,m_E,m_H)
+
+        #Save into matrix
+        E_plot[i,:] = E
+        H_plot[i,:] = H
+        print("=====================================================================")
+        print(f"Successfully computed field values! iteration: {i}/{N_t}")
+
+
+
+
+#Plotting the field values....
+plot_fields(E_plot,H_plot,N_t,injection_point,title="",save=False)
+
+
+
+
+
+
