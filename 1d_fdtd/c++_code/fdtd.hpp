9f547b91afc425273b71b8e2f3f6faf8546d3295
marvinmoyco <marvin.moyco@gmail.com>
2021-09-17 23:09:36 +0800

    Starting C++ code...

---
+++ 1d_fdtd/c++_code/fdtd.hpp
@@ -0,0 +1,22 @@
+#ifndef FDTD
+#define FDTD
+ 
+//Global variables declaration
+double c_0 = 299792458;
+double epsilon_0 = 8.8541878128E-12;
+double mu_0 = 1.25663706212E-6;
+
+//Function prototype
+void plotFields();
+void plotSingle();
+
+
+//Class declarations
+class Source(){
+
+};
+
+class Simulation(double fmax, ){
+
+};
+#endif
