e03c908262567d9a4d75b64fff5c3726c0172483
user1 <marvin.moyco@gmail.com>
2021-11-08 08:43:04 +0800

    Started working on the models

--- fdtd_web/web_interface/models.py
+++ fdtd_web/web_interface/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+
+class User(AbstractUser):
+    pass
+
+
+class Simulation(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="simulation")
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
