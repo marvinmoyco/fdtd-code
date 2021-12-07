dfb74df053fe305a03f7befded77a42cf694d341
Chulala <karlo.pio.alaan@eee.upd.edu.ph>
2021-12-08 03:02:35 +0800

    Updated convergence function

--- 1d_fdtd/c++_code/include/simulation.hpp
+++ 1d_fdtd/c++_code/include/simulation.hpp
@@ -1098,47 +1098,62 @@ class Simulation
 
         bool check_convergence()
         {
-            bool computed_l2norm[2] = {false, false};
-            double E_eps;
-            double H_eps; 
+            // Initialize matrices of overlapping region values 
+            xtensor<double,2> A_E = zeros<double>({sim_param.num_subdomains-1, sim_param.overlap_size}); 
+            xtensor<double,2> B_E = zeros<double>({sim_param.num_subdomains-1, sim_param.overlap_size});
+            xtensor<double,2> A_H = zeros<double>({sim_param.num_subdomains-1, sim_param.overlap_size}); 
+            xtensor<double,2> B_H = zeros<double>({sim_param.num_subdomains-1, sim_param.overlap_size});
+
+            // Initialize error vectors
+            xtensor<double,1> E_error = zeros<double>({sim_param.num_subdomains-1}); 
+            xtensor<double,1> H_error = zeros<double>({sim_param.num_subdomains-1});
+
+            // Initialize vectors that will store (A - B)
+            xtensor<double,1> E_sub = zeros<double>({sim_param.num_subdomains-1});
+            xtensor<double,1> H_sub = zeros<double>({sim_param.num_subdomains-1});
 
             // Initialize a vector that will store the truth values for each comparison
             xtensor<bool,1> truth_vec;
-            truth_vec.resize({sim_param.num_subdomains}); 
+            truth_vec.resize({sim_param.num_subdomains-1}); 
 
             bool isConverged = true; 
 
-            // Loops through each subdomain
-            for(int s_domain =  0; s_domain < sim_param.num_subdomains - 1; s_domain++)
+            /* 
+            Gets the values of the CURRENT subdomain's RIGHT overlapping region and the  
+            NEXT subdomain's LEFT overlapping region
+             */ 
+            for(int count =  0; count < sim_param.num_subdomains-1; count++)
             {
-                // Compute the L2 norm of the left and right overlapping regions of current subdomain 
-                computed_l2norm[0] = subdomains[s_domain].compute_L2norm("left");
-                computed_l2norm[1] = subdomains[s_domain].compute_L2norm("right");
+                view(A_E, count, all()) = subdomains[count].getOverlapValues('E',"right");
+                view(A_H, count, all()) = subdomains[count].getOverlapValues('H',"right");
 
-                // Compute the L2 norm of the E_error and H_error vectors
-                E_eps = linalg::norm(subdomains[s_domain].E_error,2);
-                H_eps = linalg::norm(subdomains[s_domain].H_error,2);
+                view(B_E, count, all()) = subdomains[count + 1].getOverlapValues('E', "left"); 
+                view(B_H, count, all()) = subdomains[count + 1].getOverlapValues('H', "left");
+            }
 
-                // Comparing to machine epsilon
-                if (E_eps <= numeric_limits<double>::epsilon() && H_eps <= numeric_limits<double>::epsilon())
+            // Subtracts the two vectors (A - B) and gets the norm; checks each element if <= epsilon
+            for(int n =  0; n < sim_param.num_subdomains-1; n++)
+            {
+                E_sub = view(A_E, n, all()) - view(B_E, n, all()); 
+                H_sub = view(A_H, n, all()) - view(B_H, n, all()); 
+
+                E_error(n) = linalg::norm(E_sub,2); 
+                H_error(n) = linalg::norm(H_sub,2); 
+
+                if (E_error(n) <= numeric_limits<double>::epsilon() && 
+                    H_error(n) <= numeric_limits<double>::epsilon()   )
                 {
-                    truth_vec[s_domain] = true; 
+                    truth_vec(n) = true; 
                 } else
                 {
-                    truth_vec[s_domain] = false; 
-                }   
-                
-                // Checks each element of the vector truth_vec
-                if (truth_vec[s_domain] == false)
+                    truth_vec(n) = false; 
+                }  
+
+                if (truth_vec(n) == false)
                 {
                     isConverged = false; 
                 }
-                
             }
-            //subdomains(0).E_error 
-            
-            //Compare final L2 norm sa machine epsilon
-            //  numeric_limits<double>::epsilon()
             return isConverged;
         }
 
